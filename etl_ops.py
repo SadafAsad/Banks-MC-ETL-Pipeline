507acf823018302c20c99e6eb506f0b089d429de
sdf <34832185+SadafAsad@users.noreply.github.com>
2024-01-25 12:06:02 -0500

    imports

---
+++ etl_ops.py
@@ -0,0 +1,9 @@
+# ETL operations on banks market cap data
+
+# import section
+import requests
+import sqlite3
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+from datetime import datetime
