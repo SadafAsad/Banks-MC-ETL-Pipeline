278dc490f18d62a7ff8ba27bfe4667566937f777
sdf <34832185+SadafAsad@users.noreply.github.com>
2024-01-25 19:07:45 -0500

    setting up docker

---
+++ script/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------
+# Airflow should follow these commands when trying to initialize scheduler
+# ------------------------------------------------------------------------
+
+set -e
+
+if [ -e "/opt/airflow/requirements.txt" ]; then
+  $(command python) pip install --upgrade pip
+  $(command -v pip) install --user -r requirements.txt
+fi
+
+if [ ! -f "/opt/airflow/airflow.db" ]; then
+  airflow db init && \
+  airflow users create \
+    --username admin \
+    --firstname admin \
+    --lastname admin \
+    --role admin \
+    --email admin@example.com \
+    --password admin
+fi
+
+$(command -v airflow) db upgrade
+
+exec airflow webserver
